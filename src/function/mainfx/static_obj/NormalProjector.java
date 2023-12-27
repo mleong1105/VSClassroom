f206d91b1e06e3e6755e16e3d304b2cab4976566
Kaize-1213 <84770198+Kaize-1213@users.noreply.github.com>
2023-12-27 12:38:55 +0800

    WIP

--- src/function/mainfx/static_obj/NormalProjector.java
+++ src/function/mainfx/static_obj/NormalProjector.java
@@ -1,11 +1,8 @@
 package function.mainfx.static_obj;
 
 import function.mainfx.ClassObj;
-import javafx.scene.control.Label;
 
 public class NormalProjector extends ClassObj implements ProjectorBehaviour {
-
-    private Label projectionLabel = new Label("");
 
     public NormalProjector() {
         super("/function/mainfx/resources/image/normalprojector.png", 200, 150, 300, 30);
--- src/function/mainfx/static_obj/TouchScreenProjector.java
+++ src/function/mainfx/static_obj/TouchScreenProjector.java
@@ -1,11 +1,8 @@
 package function.mainfx.static_obj;
 
 import function.mainfx.ClassObj;
-import javafx.scene.control.Label;
 
 public class TouchScreenProjector extends ClassObj implements ProjectorBehaviour {
-
-    private Label projectionLabel = new Label("");
 
     public TouchScreenProjector() {
         super("/function/mainfx/resources/image/touchscreenprojector.png", 150, 100, 300, 70);
